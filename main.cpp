0aa093c87ab16fe56ca60210e47a8af8bf1bd291
elphony <ushakova.polya123@gmail.com>
2024-11-06 00:43:14 +0300

    чут чут work

--- main.cpp
+++ main.cpp
@@ -3,84 +3,132 @@
 
 typedef int ListElem_t;
 
-struct ElemList {
-    ListElem_t value;
-    ListElem_t next;
-    ListElem_t prev;
-};
+const ListElem_t POISON = -666;
 
 struct list {
-    ElemList* data;
+    ListElem_t* value;
+    int* next;
+    int* prev;
     int size;
-    int head;
-    int tail;
     int free;
 };
 
-void list_ctor(list* my_list, int size) {
-    my_list->data = (ElemList*)calloc(size + 1, sizeof(ElemList));
-    my_list->size = size;
+void list_ctor(list* list, int size) {
 
-    for (int i = 1; i < my_list->size; ++i) {
-        my_list->data[i].next = i + 1;
+    // проверка указателя и размера
+
+    list->value = (ListElem_t*)calloc(size, sizeof(ListElem_t));
+    list->prev = (int*)calloc(size + 1, sizeof(int));
+    list->next = (int*)calloc(size + 1, sizeof(int));
+
+    // проверка выделенной памяти
+
+    for (int i = 1; i < size; ++i) {
+        list->value[i] = POISON;
+        list->next[i] = i + 1;
+        list->prev[i] = -1;
     }
 
-    my_list->head = 1;
-    my_list->tail = 1;
-    my_list->free = 1;
+    list->value[0] = POISON;
+    list->next[0] = 0;
+    list->prev[0] = 0; 
+
+    list->next[size - 1] = -1;
+    list->size = size;
+    list->free = 1;
 }
 
-void list_dtor(list* my_list) {
-    free(my_list->data);
-    my_list->size = 0;
-    my_list->head = 0;
-    my_list->tail = 0;
-    my_list->free = 0;
+void list_dtor(list* list) {
+    free(list->value);
+    free(list->prev);
+    free(list->next);
+    list->size = 0;
+    list->free = 0;
 }
 
-void list_insert(list* list, int index, ListElem_t value) {
+void list_insert_after(list* list, int index, ListElem_t value) {
+
+    if (list->free == -1) {
+        // все массив закончился
+        return;
+    }
 
     int new_index = list->free;
-    list->free = list->data[new_index].next;
-    list->data[new_index].value = value;
+    int old_index = list->next[index];
 
-    if (index == list->tail) {
-        list->tail = new_index;
-        list->data[new_index].next = 0;
-    }
+    list->free = list->next[new_index];
+    list->value[new_index] = value;
 
-    else {
-        list->data[new_index].next = list->data[index].next; 
-        list->data[list->data[new_index].next].prev = new_index; 
-    }
+    list->next[index] = new_index;
+    list->next[new_index] = old_index;
 
-    list->data[index].next = new_index;
-    list->data[new_index].prev = index; 
+    list->prev[new_index] = list->prev[old_index];
+    list->prev[old_index] = new_index;
 }
 
-void list_dump(list* my_list) {
+void list_insert_before(list* list, int index, ListElem_t value) {
+    
+    list_insert_after(list, list->prev[index], value);
+}
+
+void list_insert_start(list* list, ListElem_t value) {
+
+    list_insert_after(list, 0, value);
+}
+
+void list_insert_end(list* list, ListElem_t value) {
+
+    list_insert_after(list, list->prev[0], value);
+}
+
+void list_delete_index(list* list, int index) {
+    
+    list->value[index] = POISON;
+
+    int next_index = list->next[index];
+    int prev_index = list->prev[index];
+
+    list->next[prev_index] = next_index;
+    list->prev[next_index] = prev_index;
+
+    list->next[index] = list->free;
+    list->prev[index] = -1;
+    list->free = index;
+}
+
+void list_dump(list* list) {
 
     fprintf(stderr, "NEW DUMP\n");
-    fprintf(stderr, "DATA:\n");
-    for (int i = 0; i < my_list->size; ++i) {
-        fprintf(stderr, "\t[%d] = %d\n", i, my_list->data[i].value);
+    fprintf(stderr, "LIST free = %d\n", list->free);
+    fprintf(stderr, "DATA:\t\tNEXT:\t\tPREV:\n");
+    for (int i = 0; i < list->size; ++i) {
+        fprintf(stderr, "[%d] = %2d\t[%d] = %2d\t[%d] = %2d\n", i, list->value[i], i, list->next[i], i, list->prev[i]);
     }
     fprintf(stderr, "LIST:\n");
-    for (int i = 0; i < my_list->size; ++i) {
-        fprintf(stderr, "\t[%d] = %d\n", i, my_list->data[my_list->data[i].next].value);
+    int i = list->next[0];
+    while (i != 0) {
+        fprintf(stderr, "\t[%d] = %d\n", i, list->value[i]);
+        i = list->next[i];
     }
 }
 
 int main() {
-    list my_list = {};
+    list list = {};
 
-    list_ctor(&my_list, 10);
-    list_dump(&my_list);
-    list_insert(&my_list, 1, 10);
-    list_dump(&my_list);
-    list_insert(&my_list, 2, 20);
-    list_dump(&my_list);
-    list_insert(&my_list, 2, 15);
-    list_dump(&my_list);
-    list_dtor(&my_list);
+    list_ctor(&list, 9);
+    list_dump(&list);
+    
+    list_insert_after(&list, 0, 10);
+    list_dump(&list);
+
+    list_insert_after(&list, 1, 20);
+    list_dump(&list);
+
+    list_insert_after(&list, 1, 15);
+    list_dump(&list);
+
+    list_insert_start(&list, 5);
+    list_dump(&list);
+
+    list_dtor(&list);
 }
